0dbbd9bbc45caffe404e4eb875dc9d6c4a0549c7
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-04 15:52:15 +0800

    oh

---
+++ precache-manifest.7e230c712c3e1352156a2ae82c27e2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00cd04ec41db52e7ee6aed82cf0a060",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d7e653cd6e5ef798344",
+    "url": "./static/css/main.f49f1b21.chunk.css"
+  },
+  {
+    "revision": "e530c015b2d2dc687fbb",
+    "url": "./static/js/2.44448fc1.chunk.js"
+  },
+  {
+    "revision": "9d7e653cd6e5ef798344",
+    "url": "./static/js/main.209163bd.chunk.js"
+  },
+  {
+    "revision": "03233c5893d19cb279d5",
+    "url": "./static/js/runtime~main.637245ba.js"
+  }
+]);
