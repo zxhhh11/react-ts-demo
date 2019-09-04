19ca7b9dc1dd16a3c918249eb1ad422d0a6ee039
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-04 15:20:07 +0800

    test

---
+++ precache-manifest.070c6c8e2bb224d8885582188f4551d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad9d31db86c2fc91ccfa943115069fa",
+    "url": "/myhook/index.html"
+  },
+  {
+    "revision": "9d7e653cd6e5ef798344",
+    "url": "/myhook/static/css/main.f49f1b21.chunk.css"
+  },
+  {
+    "revision": "e530c015b2d2dc687fbb",
+    "url": "/myhook/static/js/2.44448fc1.chunk.js"
+  },
+  {
+    "revision": "9d7e653cd6e5ef798344",
+    "url": "/myhook/static/js/main.209163bd.chunk.js"
+  },
+  {
+    "revision": "0f5614a4822b97c30b8f",
+    "url": "/myhook/static/js/runtime~main.184bfd52.js"
+  }
+]);
