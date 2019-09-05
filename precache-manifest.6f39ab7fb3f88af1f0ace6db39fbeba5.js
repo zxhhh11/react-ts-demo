7b070383fba9fc6b4173120026dc3485bfe3ea17
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-05 10:28:51 +0800

    change the route is hook , comment out others route

---
+++ precache-manifest.6f39ab7fb3f88af1f0ace6db39fbeba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea64b69de662909491991f20f249931",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae9f4ac36d19c1e4d96f",
+    "url": "./static/css/main.f49f1b21.chunk.css"
+  },
+  {
+    "revision": "339fb306a0611446486b",
+    "url": "./static/js/2.95766b44.chunk.js"
+  },
+  {
+    "revision": "ae9f4ac36d19c1e4d96f",
+    "url": "./static/js/main.41051f92.chunk.js"
+  },
+  {
+    "revision": "03233c5893d19cb279d5",
+    "url": "./static/js/runtime~main.637245ba.js"
+  }
+]);
