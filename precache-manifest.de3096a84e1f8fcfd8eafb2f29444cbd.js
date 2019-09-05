297f2617343c4bc38f16758538b672b17d18c749
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-05 09:55:59 +0800

    test 05 3 homepage:'./'  add history in Router tag

---
+++ precache-manifest.de3096a84e1f8fcfd8eafb2f29444cbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdb80cba261a7a6e2f4d24f63db815a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7099834353aec25dbc1b",
+    "url": "./static/css/main.f49f1b21.chunk.css"
+  },
+  {
+    "revision": "1dd38729359142c7e523",
+    "url": "./static/js/2.8fa005d7.chunk.js"
+  },
+  {
+    "revision": "7099834353aec25dbc1b",
+    "url": "./static/js/main.34645648.chunk.js"
+  },
+  {
+    "revision": "03233c5893d19cb279d5",
+    "url": "./static/js/runtime~main.637245ba.js"
+  }
+]);
