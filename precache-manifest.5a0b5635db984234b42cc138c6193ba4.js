4e5c22b89811610b2995888e8165d59051f6202d
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-05 10:58:12 +0800

    test 05 6 change the antdesigndemo

---
+++ precache-manifest.5a0b5635db984234b42cc138c6193ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64071cf19ce04b824d706a373e9d5db2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abce268fab6071382879",
+    "url": "./static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "3d0971f377c0a3b2a46c",
+    "url": "./static/css/main.e7b48627.chunk.css"
+  },
+  {
+    "revision": "abce268fab6071382879",
+    "url": "./static/js/2.88780845.chunk.js"
+  },
+  {
+    "revision": "3d0971f377c0a3b2a46c",
+    "url": "./static/js/main.2c6e6bb7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a751ef4b304172199ebc13e518be9197.js"
+  "./precache-manifest.5a0b5635db984234b42cc138c6193ba4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
