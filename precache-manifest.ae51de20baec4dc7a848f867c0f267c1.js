d615ca6992ea7cdd5536abde73e54d989cfbff15
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-09-05 14:07:46 +0800

    change homepage is './'

---
+++ precache-manifest.ae51de20baec4dc7a848f867c0f267c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c25262201026de0cbf12b74d9afabc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72e2ff9bc37e0d88b4cd",
+    "url": "./static/css/main.f49f1b21.chunk.css"
+  },
+  {
+    "revision": "15415beb924ee146c84a",
+    "url": "./static/js/2.bdc5aacc.chunk.js"
+  },
+  {
+    "revision": "72e2ff9bc37e0d88b4cd",
+    "url": "./static/js/main.d716b86d.chunk.js"
+  },
+  {
+    "revision": "03233c5893d19cb279d5",
+    "url": "./static/js/runtime~main.637245ba.js"
+  }
+]);
